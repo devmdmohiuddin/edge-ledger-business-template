dea34ebcfbeaa17ec6ef7afc11865a36e2c6ae38
mohiuddinmohi <mohiuddin0240@gmail.com>
2022-05-24 17:50:36 +0600

    style: format updated

--- js/main.js
+++ js/main.js
@@ -1,37 +1,37 @@
 // Initialize and add the map
 function initMap() {
-    // Your location
-    const loc = { lat: 23.810331, lng: 90.412521 };
-    // Centered map on location
-    const map = new google.maps.Map(document.querySelector('.map'), {
-        zoom: 14,
-        center: loc,
-    });
-    // The marker, position at location
-    const marker = new google.maps.Marker({ position: loc, map: map });
+  // Your location
+  const loc = { lat: 23.810331, lng: 90.412521 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector(".map"), {
+    zoom: 14,
+    center: loc,
+  });
+  // The marker, position at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
 // Sticky menu background
-window.addEventListener('scroll', function () {
-    if (window.scrollY > 150) {
-        document.querySelector('#navbar').style.opacity = 0.9
-    } else {
-        document.querySelector('#navbar').style.opacity = 1 
-    }
-})
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 150) {
+    document.querySelector("#navbar").style.opacity = 0.9;
+  } else {
+    document.querySelector("#navbar").style.opacity = 1;
+  }
+});
 
 // Smooth Scrolling
-$('#navbar a, .btn').on('click', function (event) {
-    if (this.hash !== '') {
-        event.preventDefault();
+$("#navbar a, .btn").on("click", function (event) {
+  if (this.hash !== "") {
+    event.preventDefault();
 
-        const hash = this.hash;
+    const hash = this.hash;
 
-        $('html, body').animate(
-            {
-                scrollTop: $(hash).offset().top - 55,
-            },
-            800
-        );
-    }
+    $("html, body").animate(
+      {
+        scrollTop: $(hash).offset().top - 55,
+      },
+      800
+    );
+  }
 });
